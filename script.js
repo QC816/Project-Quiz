7c3037c3e5bd3be4159f285cbf2306193d769c9b
Quinny Chiang <quinnychiang@rogers.com>
2019-05-10 13:40:04 -0400

    upload all work-in-progress files

---
+++ script.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+
+	$("form").on("submit", function(){
+
+  	event.preventDefault();
+		console.log(event);
+
+		console.log("submitted!!");
+
+		const crustValue = $("input[name='crust']:checked").val();
+
+		console.log(crustValue);
+
+		const sauceValue = $("input[name='sauce']:checked").val();
+		console.log(sauceValue);
+
+    let cheeseValue = [];
+		$("input[name='cheese']:checked").each(function() {
+   			console.log(this.value);
+        cheeseValue.push(this.value);
+		});
+    console.log(cheeseValue.toString());
+
+    let toppingsValue = [];
+		$("input[name='toppings']:checked").each(function() {
+   			console.log(this.value);
+        toppingsValue.push(this.value);
+		});
+    console.log(toppingsValue.toString());
+
+    const extraValue = $("input[name='extra']:checked").val();
+    console.log(extraValue);
+
+		let counter = $(".counter");
+			counter = 0;
+
+    if (crustValue === "Thick crust"){
+			counter = counter + 1;
+		}
+    $(".counter").text(counter);
+
+		if (sauceValue === "Tomato sauce"){
+			counter = counter + 1;
+		}
+    $(".counter").text(counter);
+
+		if (cheeseValue.toString() === "Cheddar,Parmesan"){
+			counter = counter + 2;
+		}; 
+		$(".counter").text(counter);
+
+    if (toppingsValue.toString() === "Pepperoni,Red onions,Fresh green peppers,Fresh mushrooms,Italian sausage"){
+      counter = counter + 5;
+    };
+    $("counter").text(counter);
+
+    if (extraValue === "Cracked red pepper"){
+      counter = counter + 1;
+    }
+    $(".counter").text(counter);
+
+    if (counter === 10) {
+      $(".congrats").fadeIn();
+    }
+	});
+});
+
+// This can also work for multiple values:
+// function arraysEqual(arr1, arr2) {
+//     if(arr1.length !== arr2.length)
+//         return false;
+//     for(var i = arr1.length; i--;) {
+//         if(arr1[i] !== arr2[i])
+//             return false;
+//     }
+
+//     return true;
+// }
